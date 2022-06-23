cd94702c6edb8bde3949b4d91d3aab008c373a97
YHKwon <ssugy2@gmail.com>
2022-06-23 18:01:57 +0900

    #5 C#폴더 생성 및 문제풀기
    1330 1550 2558 2741 2752
    풀이 완료

---
+++ CSharp/BakJoon/Proj1330/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String? input = Console.ReadLine();
+
+int num1 = int.Parse(input.Split(' ')[0]);
+int num2 = int.Parse(input.Split(' ')[1]);
+
+if (num1 > num2)
+{
+    Console.WriteLine(">");
+}else if (num1 == num2)
+{
+    Console.WriteLine("==");
+}
+else
+{
+    Console.WriteLine("<");
+}
+
+/*
+ * 다른 개발자의 숏코딩
+    var a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+    Console.Write(a[0] > a[1] ? ">" : a[0] < a[1] ? "<" : "==");
+*/
---
+++ CSharp/BakJoon/Proj2741/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 문제
+자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+*/
+
+using System.Text;
+
+int num = int.Parse(Console.ReadLine());
+StringBuilder str = new StringBuilder();
+
+for (int i = num; i >= 1; i--)
+{
+    str.Append(i + "\n");
+}
+
+Console.WriteLine(str);
+
+/*
+ * using System;
+using System.Linq;
+
+Console.WriteLine(string.Join(Console.Out.NewLine, Enumerable.Range(1, int.Parse(Console.ReadLine()))));
+
+# 링큐를 이용해서 작성함.
+*/
