3c744fcadca2cac53ba50cd9109b2a47dd1d58fe
YHKwon <ssugy2@gmail.com>
2022-06-30 12:10:25 +0900

    #5 4562번까지 풀이 완료. 이 후는 다른방식으로 git올릴 예정
    여기까지는 프로젝트 단위로 올리고, 이 후부터는 프로젝트에서 테스트 끝난 cs파일만 올릴 예정.
    
    vs에서 직접하면 프로젝트 단위로 git연결해야되는 부분이 있어서.. 내가 정확하게 몰라서 그러는 것 일 수도 있습니다. 아무튼 vs code에서 올릴 예정

---
+++ CSharp/BakJoon/Basic Algorithm/Proj4101/Program.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * 문제
+두 양의 정수가 주어졌을 때, 첫 번째 수가 두 번째 수보다 큰지 구하는 프로그램을 작성하시오.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 두 정수가 주어진다. 두 수는 백만보다 작거나 같은 양의 정수이다. 입력의 마지막 줄에는 0이 두 개 주어진다.
+
+출력
+각 테스트 케이스마다, 첫 번째 수가 두 번째 수보다 크면 Yes를, 아니면 No를 한 줄에 하나씩 출력한다.
+*/
+
+int num1 = -1;
+int num2 = -1;
+
+while (num1 != 0 || num2 != 0)
+{
+    string? inputStr = Console.ReadLine();  // 널이 나올수 없는데 널이야기를 하는게 이해가 안가긴한다.
+    num1 = int.Parse(inputStr.Split()[0]);
+    num2 = int.Parse(inputStr.Split()[1]);
+
+    if (num1 ==0 && num2 ==0)
+    {
+        break;
+    }
+
+    if (num1 > num2)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
