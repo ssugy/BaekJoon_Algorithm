8b9f40f2aed7ba6f1fe38a0f8ab41f6812e98e9b
YHKwon <ssugy2@gmail.com>
2023-01-01 23:18:35 +0900

    링큐 Count사용. 빈칸을 Trim에 split하면 1이 나오는 것은 처음 알게됨.(신기하네요)

---
+++ CSharp/BakJoon/CodingStudy/CodingStudy/BJ1152.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingStudy
+{
+    internal class BJ1152
+    {
+        public static void Main()
+        {
+            // 내가 제출한 정답
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            string str = sr.ReadLine();
+            if (str.Trim() != string.Empty)
+            {
+                Console.WriteLine(str.Trim().Split().Length);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+            // Trim 처리를 해도, split하면 길이가 1이 나온다.(이건 처음 알았네)
+            Console.WriteLine(string.Empty.Split());
+            int cnt = 0;
+            foreach (var item in Console.ReadLine().Trim().Split())
+            {
+                Console.WriteLine(item + (cnt++));
+            }
+
+            // 숏코딩 정답 - 그냥 Count를 사용하면, 빈칸일경우 정상적인 답이 안나옴 처리가 안됨.
+            Console.WriteLine(Console.ReadLine().Split().Count(x => x.Length > 0));
+        }
+    }
+}
--- CSharp/BakJoon/CodingStudy/CodingStudy/BJ25372.cs
+++ CSharp/BakJoon/CodingStudy/CodingStudy/BJ25372.cs
@@ -7,26 +7,28 @@ using System.Threading.Tasks;
 
 namespace CodingStudy
 {
-    internal class BJ25304
+    internal class BJ25372
     {
         public static void _Main()
         {
             var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-            int totalPrice = int.Parse(sr.ReadLine());
             int cnt = int.Parse(sr.ReadLine());
             for (int i = 0; i < cnt; i++)
             {
-                string price = sr.ReadLine();
-                totalPrice -= (int.Parse(price.Split()[0]) * int.Parse(price.Split()[1]));
+                string str = sr.ReadLine();
+                if (str.Length > 5 && str.Length < 10)
+                {
+                    sw.WriteLine("yes");
+                }
+                else
+                {
+                    sw.WriteLine("no");
+                }
             }
-
-            if (totalPrice == 0)
-                sw.Write("Yes");
-            else
-                sw.Write("No");
             sr.Close();
             sw.Close();
+
         }
     }
 }
